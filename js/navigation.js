260c92a7000fdc020cc39574e825b6b57ea11e8a
Alber <alberdsp@gmail.com>
2023-06-17 22:14:01 +0200

    añadida función de carrito

--- js/navigation.js
+++ js/navigation.js
@@ -19,18 +19,89 @@ function showSlide(slideIndex) {
     });
 }
 
+// establece cual es el siguiente slide
 function nextSlide() {
     currentSlide = (currentSlide + 1) % slides.length;
+
     showSlide(currentSlide);
 }
 
+// intervalo en milisegundos
 function startSlider() {
     slideInterval = setInterval(nextSlide, 3000);
 }
 
-function stopSlider() {
-    clearInterval(slideInterval);
+
+
+// Iniciamos el sliderlist
+startSlider();
+
+
+
+
+// Función de añadir al carrito
+
+
+
+const addCarritoBoton = Array.from(document.getElementsByClassName('addcarrito'));
+const iconoCarrito = document.getElementById('icono-carrito');
+const listaCarritoContainer = document.getElementById('lista-carrito');
+const totalListaCarrito = document.getElementById('total-carrito');
+
+
+
+let listaCarrito = [];
+let totalCarrito = 0;
+
+function updatetotalCarrito() {
+  totalListaCarrito.textContent = `Total: ${totalCarrito} €`;
 }
 
-// Iniciamos el slider
-startSlider();
+function addToCart(producto) {
+  listaCarrito.push(producto);
+  totalCarrito += producto.precio;
+  updatetotalCarrito();
+  displaylistaCarrito();
+  console.log(totalCarrito);
+  console.log(listaCarrito);
+}
+
+function displaylistaCarrito() {
+  listaCarritoContainer.innerHTML = '';
+  listaCarrito.forEach(item => {
+    console.log(item.name);
+    let cartItemElement = document.createElement('div');
+    cartItemElement.textContent = item.nombre;
+    listaCarritoContainer.appendChild(cartItemElement);
+    console.log(cartItemElement);
+  });
+}
+
+
+addCarritoBoton.forEach(button => {
+  button.addEventListener('click', () => {
+
+
+    let card = document.querySelector('.card');
+    let nombreProducto = card.querySelector('.nombre').textContent;
+    let precioProducto = card.querySelector('.precio').textContent;
+    precioProducto =  precioProducto.substring(0,precioProducto.trim().length -1);
+   // precioProducto = parseFloat(precioProducto);
+    nombreProducto = nombreProducto.substring(0,20);
+    precioProducto= parseFloat(precioProducto);
+    console.log(nombreProducto);
+    console.log(precioProducto);
+    
+    let producto = {
+      nombre: nombreProducto,
+      precio: precioProducto,
+    };
+    addToCart(producto);
+    displaylistaCarrito();
+  });
+});
+
+iconoCarrito.addEventListener('click', () => {
+  listaCarritoContainer.classList.toggle('show');
+});
+
