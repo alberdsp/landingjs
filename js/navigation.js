d711ba5c3aa556018ade2f0037561dc2a91818e6
Alber <alberdsp@gmail.com>
2023-06-17 11:02:43 +0200

    banner implementado

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,36 @@
+// navigation.js
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const slider = document.querySelector(".slider");
+const slides = Array.from(slider.querySelectorAll("img.slide"));
+let currentSlide = 0;
+let slideInterval;
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
+// creamos una función que recorre las imágenes
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+function showSlide(slideIndex) {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+            slide.classList.add("active");
+        } else {
+            slide.classList.remove("active");
+        }
+    });
+}
 
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
 
+function startSlider() {
+    slideInterval = setInterval(nextSlide, 3000);
+}
+
+function stopSlider() {
+    clearInterval(slideInterval);
+}
+
+// Iniciamos el slider
+startSlider();
